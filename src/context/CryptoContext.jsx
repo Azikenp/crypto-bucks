961f36525de011fc9986b80f4899cf03bfbbb136
Azikenp <azikenp26@gmail.com>
2023-09-03 20:56:06 +0100

    integrating API

--- src/context/CryptoContext.jsx
+++ src/context/CryptoContext.jsx
@@ -1,27 +1,31 @@
-import { createContext, useState } from "react"
-
+import { createContext, useLayoutEffect, useState } from "react";
 
 //create context object
 export const CryptoContext = createContext({});
 
-
-
 //create the provide object
-export const CryptoProvider = ({children}) => {
-    const [cryptoData, setCryptoData] = useState();
+export const CryptoProvider = ({ children }) => {
+  const [cryptoData, setCryptoData] = useState();
 
-    const getCryptoData = async () => {
-        try{
-            
-        } catch(error){
-            console.log(error)
-        }
+  const getCryptoData = async () => {
+    try {
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d&locale=en`
+      )
+        .then((res) => res.json())
+        .then((json) => json);
+
+        console.log(data);
+        setCryptoData(data)
+
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  useLayoutEffect(() => {
+    getCryptoData()
+  }, [])
 
-    return(
-        <CryptoContext.Provider value={{}}>
-            {children}
-        </CryptoContext.Provider>
-    )
-}
+  return <CryptoContext.Provider value={{cryptoData}}>{children}</CryptoContext.Provider>;
+};
