2107e4eec36082968fa718cb5c59886d899d101e
Azikenp <azikenp26@gmail.com>
2023-09-09 17:49:37 +0100

    local storage included

---
+++ src/context/StorageContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useLayoutEffect, useState } from "react";
+
+//create context object
+export const StorageContext = createContext({});
+
+//create the provide object
+export const StorageProvider = ({ children }) => {
+  const [allCoins, setAllCoins] = useState([]);
+
+  const saveCoin = (coinId) => {
+    let oldCoins = JSON.parse(localStorage.getItem("coins"))
+
+    if (oldCoins.includes(coinId)){
+        return null;
+    } else {
+        let newCoin = [...oldCoins, coinId];
+        setAllCoins(newCoin)
+        localStorage.setItem("coins", JSON.stringify(newCoin))
+    }
+  }
+
+  useLayoutEffect(() => {
+    let isThere = JSON.parse(localStorage.getItem("coin")) || false;
+
+    if(!isThere){
+        //set the local storage with an empty array
+        localStorage.setItem("coins", JSON.stringify([]));
+        
+    } else{
+        //set the state with the curremt values from local storage
+        let totalCoin = JSON.parse(localStorage.getItem("coin"));
+        setAllCoins(totalCoin)
+    }
+  }, []);
+
+  return (
+    <StorageContext.Provider
+      value={{
+        saveCoin
+      }}
+    >
+      {children}
+    </StorageContext.Provider>
+  );
+};
